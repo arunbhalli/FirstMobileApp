475db3d5259139e427a312c664c890fad54939c4
arunbhalli <tomarbhalli@gmail.com>
2021-05-25 17:30:26 +0200

    Fetch fake news and displaying individual articles rename of FirstScreen

---
+++ screens/Article.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+const Article = ({ article, navigation }) => {
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('Second Screen', {article: article });
+      }}>
+      <Image source={{ uri: article.image }} style={styles.image} />
+      <View style={styles.card}>
+        <Text style={styles.title}>{article.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default Article;
+
+const styles = StyleSheet.create({
+  card: {
+    position: 'relative',
+    width: Dimensions.get('window').width,
+    padding: 7,
+    paddingTop: 2,
+  
+  },
+  title: {
+    fontSize: '12px',
+    paddingBottom: '10px',
+  },
+  image: {
+    height: 300,
+    width: Dimensions.get('window').width,
+    marginTop: 40,
+  },
+});
